cf1f01e8615008d894f606c9b2382f47fe471e27
shaafiqe <shaafiqe2003@gmail.com>
2023-11-06 00:52:48 +0530

    remote sharing test

--- Running Yolo/yolo_basics.py
+++ Running Yolo/yolo_basics.py
@@ -4,3 +4,5 @@ import cv2
 model = YOLO('C:/Users/SOMANATH/Desktop/cv/Yolo-Weights/yolov8l.pt')
 results = model("C:/Users/SOMANATH/Desktop/cv/Running Yolo/Images/3.jpg", show = True)
 cv2.waitKey(0)
+
+print("test for remote sharing")
