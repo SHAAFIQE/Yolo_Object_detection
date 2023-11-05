404cf5fe44ca11f76fedd898126a3f7fe8dc2780
shaafiqe <shaafiqe2003@gmail.com>
2023-11-06 00:17:06 +0530

    Initaial Commit

---
+++ Running Yolo/yolo_basics.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('C:/Users/SOMANATH/Desktop/cv/Yolo-Weights/yolov8l.pt')
+results = model("C:/Users/SOMANATH/Desktop/cv/Running Yolo/Images/3.jpg", show = True)
+cv2.waitKey(0)
