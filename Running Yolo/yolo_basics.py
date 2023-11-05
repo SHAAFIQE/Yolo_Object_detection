de1a1e687009d39d159776bf6992f033c8ed4656
shaafiqe <shaafiqe2003@gmail.com>
2023-11-06 00:41:53 +0530

    test-2 commit

--- Running Yolo/yolo_basics.py
+++ Running Yolo/yolo_basics.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO('C:/Users/SOMANATH/Desktop/cv/Yolo-Weights/yolov8l.pt')
-results = model("C:/Users/SOMANATH/Desktop/cv/Running Yolo/Images/2.jpg", show = True)
+results = model("C:/Users/SOMANATH/Desktop/cv/Running Yolo/Images/3.jpg", show = True)
 cv2.waitKey(0)
